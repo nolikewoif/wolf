aeda2b209d4d328dfe422e16067668cdbaf9ee87
nolikewoif <nolikewoif@163.com>
2019-08-27 10:10:41 +0800

    提交spring-boot-quartz项目

--- src/main/java/com/itstyle/quartz/Application.java
+++ src/main/java/com/itstyle/quartz/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	private static final Logger logger = Logger.getLogger(Application.class);
-	
+
 	public static void main(String[] args) throws InterruptedException, SchedulerException {
 		SpringApplication.run(Application.class, args);
 		logger.info("项目启动 ");
